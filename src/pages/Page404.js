c1a6f52638333f28aeddeb0402586c5480585b41
Edlee Ducay <ducay@ualberta.ca>
2022-02-20 14:44:16 -0700

    feat: 404 page

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Button, Typography, Container } from '@mui/material';
 
 export default function Page404() {
-  return <div>404</div>;
+  return (
+      <Container>
+          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' , mt: 10}}>
+              <Typography variant="h1" paragraph>
+                404
+              </Typography>
+              <Typography variant="h3" paragraph>
+                Sorry, page not found!
+              </Typography>
+              
+            <Typography>
+              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL?
+              Be sure to check your spelling.
+            </Typography>
+
+            <Button to="/" size="large" variant="contained" component={RouterLink} sx={{mt: 3}}>
+              Go to Home
+            </Button>
+          </Box>
+      </Container>
+  );
 }
